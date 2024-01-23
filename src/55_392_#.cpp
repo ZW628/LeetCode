a59b67689046f627931ca28718f9edf245286db1
zhiwei <ZW628>
2024-01-23 11:00:06 +0800

    update

--- src/55_392_#.cpp
+++ src/55_392_#.cpp
@@ -8,11 +8,14 @@ public:
     bool isSubsequence(string s, string t) {
         vector<vector<int>> dp(s.size() + 1, vector<int>(t.size() + 1, 0));
         for (int i = 1; i <= s.size(); i++) {
+            cout << "i:" << i << endl;
             for (int j = 1; j <= t.size(); j++) {
+                cout << "  j:" << j << endl;
                 if (s[i - 1] == t[j - 1])
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 else
                     dp[i][j] = dp[i][j - 1];
+                cout << "    dp[" << i << "][" << j << "]=" << dp[i][j] << endl;
             }
         }
         if (dp[s.size()][t.size()] == s.size()) return true;
@@ -22,7 +25,7 @@ public:
 
 int main() {
     Solution solution;
-    string s = "abc", t = "ahbgdc";
+    string s = "acb", t = "ahbgdc";
     bool result = solution.isSubsequence(s, t);
     cout << boolalpha << result;
 
