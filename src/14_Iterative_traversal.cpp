0046771c4cecc9baba767a138f479082ed0cab21
ZW628 <zhiweicome@163.com>
2024-01-30 12:03:35 +0800

    update

---
+++ src/14_Iterative_traversal.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+
+#include "TreeNode.hpp"
+using namespace std;
+
+// 迭代_前序遍历
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        stack<TreeNode *> st;
+        vector<int> result;
+        if (root == nullptr) return result;
+        st.push(root);
+        while (!st.empty()) {
+            TreeNode *node = st.top();
+            st.pop();
+            result.push_back(node->val);
+            if (node->right) st.push(node->right);
+            if (node->left) st.push(node->left);
+        }
+        return result;
+    }
+};
+
+int main() {
+    TreeNode *root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+
+    Solution solution;
+    vector<int> result = solution.preorderTraversal(root);
+
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i];
+        if (i < result.size() - 1) cout << ",";
+    }
+
+    cin.get();
+    return 0;
+}
